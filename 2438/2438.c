81629e95665b506b572c45412bbbd072450f9f09
JK Lim <artherlim@gmail.com>
2017-09-13 15:00:48 +0900

    Print

---
+++ 2438/2438.c
@@ -0,0 +1,16 @@
+#define CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int N = 0;
+	int i;
+	int j;
+	scanf("%d", &N);
+	for (j = 0; j < N; j++) {
+		for (i = 0; i <= j; i++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+	return 0;
+}
