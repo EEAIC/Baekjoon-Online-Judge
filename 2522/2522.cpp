b86cb7d7d20946b19c6e62b6be40b00f9e40c324
JK Lim <artherlim@gmail.com>
2019-08-07 16:50:10 +0900

    Print

---
+++ 2522/2522.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+
+using namespace std;
+
+class starDraw
+{
+public:
+	starDraw(int N);
+	~starDraw();
+	int** arr;
+	int N;
+	void Draw_print();
+private:
+	void Draw();
+};
+
+starDraw::starDraw(int N)
+{
+	starDraw::N = N;
+	arr = new int*[N];
+	for (int i = 0; i < N; i++) {
+		arr[i] = new int[2 * N - 1];
+		for (int j = 0; j < 2 * N - 1; j++) {
+			arr[i][j] = 0;
+		}	
+	}
+
+}
+
+
+starDraw::~starDraw()
+{
+}
+
+void starDraw::Draw() {
+	for (int i = 0; i < 2 * N - 1; i++) {
+		for (int j = 0; j < N; j++) {
+			if (-1 * j + N - 1 <= i && i <= j + N - 1) {
+				arr[j][i] = 1;
+			}
+		}
+	}
+}
+
+void starDraw::Draw_print() {
+	Draw();
+	for (int i = 0; i < 2 * N - 1; i++) {
+		for (int j = 0; j < N; j++) {
+			if (arr[j][i]) cout << "*";
+			else cout << " ";
+
+			if (j == N - 1) {
+				cout << "\n";
+			}
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N;
+	cin >> N;
+	starDraw star(N);
+	star.Draw_print();
+
+	return 0;
+}
