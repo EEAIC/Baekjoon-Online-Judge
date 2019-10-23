a4537a23a14bb4b33c0aea44b6d6639ebccfba7b
JK Lim <artherlim@gmail.com>
2019-10-23 10:57:08 +0900

    Greedy algorithm

---
+++ 11399/11399.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int N;
+    int rst = 0;
+    cin >> N;
+
+    int* arr = new int[N];
+
+    for (int i = 0; i < N; i++) {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + N);
+
+    for (int i = 0; i < N; i++) {
+        rst += arr[i] * (N - i);
+    }
+    
+    cout << rst;
+
+    return 0;
+}
