55d866c9743645df5ea7d6f1dcb0de2bca16c99f
LIFOsitory <artherlim@gmail.com>
2019-12-13 15:22:13 +0900

    Arithmetic operation

---
+++ 18005/18005.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    if (n % 2) {
+        cout << 0;
+    } else {
+        if ((n * (n - 1) / 2) % 2) {
+            cout << 1;
+        } else {
+            cout << 2;
+        }
+    }
+
+    return 0;
+}
